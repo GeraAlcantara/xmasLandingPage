507e9e54ba428f52fbce79a0c98f3e5f7a30e3df
Gerardo Alcantara <32178037+GeraAlcantara@users.noreply.github.com>
2022-11-03 00:58:12 -0500

    addin svg wave

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import { TbArrowBigDownLines } from "react-icons/tb";
 import Housexmas from "../assets/housexmas.png";
 import ToastNotification from "./ToastNotification";
+import Wavebanner from "../assets/wavebanner.svg";
+import WavesSvg from "./WavesSvg";
 
 function Hero() {
   return (
-    <div className='bg-gradient-to-b from-brandPurple-700 to-brandPurple-600 py-4 md:py-16 min-h-[calc(100vh-56px)]  flex flex-col justify-around '>
+    <div className='bg-gradient-to-b from-brandPurple-700 to-brandPurple-600 pt-4 md:pt-16 min-h-[calc(100vh-56px)]  flex flex-col justify-around '>
       <div className='container m-auto px-4 md:px-8 xl:px-0 text-white flex flex-col-reverse lg:flex-row lg:items-center lg:justify-between'>
         <div className='flex flex-col gap-4  md:p-10 lg:p-0 w-full xl:1/2'>
           <div className='flex justify-center p-6 md:p-0 md:justify-start'>
@@ -26,10 +28,13 @@ function Hero() {
         <div className='w-full md:w-3/4 p-4 m-auto relative'>
           <img src={Housexmas} alt='3d House with christmas lights' className='w-full h-auto' />
         </div>
-        <div className='hidden lg:absolute -bottom-14 lg:bottom-4 right-8 lg:flex flex-col gap-4'>
+        <div className='hidden lg:absolute -bottom-14 lg:bottom-24 right-8 lg:flex flex-col gap-4'>
           <ToastNotification />
           <ToastNotification updatetype='previuos' date='24 000m above Norway, March 5.2, T-12min' timepassed={10} />
         </div>
+      </div>
+      <div className='relative bottom-0 '>
+        <WavesSvg />
       </div>
     </div>
   );
