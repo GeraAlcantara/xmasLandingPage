6988f24fe0c3018a901ad3dc1087e0e10873e8d1
Gerardo Alcantara <32178037+GeraAlcantara@users.noreply.github.com>
2022-11-03 23:29:48 -0500

    cardsquotes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,49 @@
 import "./App.css";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import { RiDoubleQuotesL } from "react-icons/ri";
 
 function App() {
   return (
     <div>
       <Header />
       <Hero />
+      <section className='pt-[200px] md:pt-[100px] xl:pt-[140px]  bg-brandPurple-50'>
+        <div className='px-2 text-center'>
+          <h2 className='text-3xl text-brandPurple-700 font-semibold mb-2'>First-a few short poems for when you're in a bind!</h2>
+          <p className='text-gray-900 '>Copy or screenshot these and store them in your phone</p>
+        </div>
+        {/* card quotes */}
+        <div className='flex flex-col md:flex-row flex-wrap items-center justify-center p-4 xl:p-16 gap-8 w-full '>
+          {/* Card 1 */}
+          <div className='bg-white relative shadow-2xl shadow-brandPurple-200 p-8 max-w-[400px] min-h-[390px] rounded-t-2xl rounded-br-2xl speech-bubble mb-8 '>
+            <blockquote cite='Shirley Sallay p-4'>
+              <RiDoubleQuotesL className='text-9xl text-brandPurple-600 font-serif leading-none'>"</RiDoubleQuotesL>
+              <p className='text-xl mb-4'>Christmas now surround us, Happiness is everywhere Our hands are busy with many tasks As carols fill the air. </p>
+              <cite className='text-gray-400 text-lg '>Shirley Sallay</cite>
+              <p className='text-brandPurple-700 font-bold mt-4'>TAP CARD TO COPY</p>
+            </blockquote>
+          </div>
+          {/* Card 2 */}
+          <div className='bg-white relative shadow-2xl shadow-brandPurple-200 p-8 max-w-[400px] min-h-[390px] rounded-t-2xl rounded-br-2xl speech-bubble mb-8 '>
+            <blockquote cite='Shirley Sallay p-4'>
+              <RiDoubleQuotesL className='text-9xl text-brandPurple-600 font-serif leading-none'>"</RiDoubleQuotesL>
+              <p className='text-xl mb-4'>Christmas now surround us, Happiness is everywhere Our hands are busy with many tasks As carols fill the air. </p>
+              <cite className='text-gray-400 text-lg '>Shirley Sallay</cite>
+              <p className='text-brandPurple-700 font-bold mt-4'>TAP CARD TO COPY</p>
+            </blockquote>
+          </div>
+          {/* Card 3 */}
+          <div className='bg-white relative shadow-2xl shadow-brandPurple-200 p-8 max-w-[400px] min-h-[390px] rounded-t-2xl rounded-br-2xl speech-bubble mb-8 '>
+            <blockquote cite='Shirley Sallay p-4'>
+              <RiDoubleQuotesL className='text-9xl text-brandPurple-600 font-serif leading-none'>"</RiDoubleQuotesL>
+              <p className='text-xl mb-4'>Christmas now surround us, Happiness is everywhere Our hands are busy with many tasks As carols fill the air. </p>
+              <cite className='text-gray-400 text-lg '>Shirley Sallay</cite>
+              <p className='text-brandPurple-700 font-bold mt-4'>TAP CARD TO COPY</p>
+            </blockquote>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,13 +4,15 @@ import Housexmas from "../assets/housexmas.png";
 import ToastNotification from "./ToastNotification";
 import Wavebanner from "../assets/wavebanner.svg";
 import WavesSvg from "./WavesSvg";
+import Cookie from "../assets/cookie.png";
+import Letter from "../assets/Letter.png";
 
 function Hero() {
   return (
-    <div className='bg-gradient-to-b from-brandPurple-700 to-brandPurple-600 pt-4 md:pt-16 min-h-[calc(100vh-56px)]  flex flex-col justify-around '>
-      <div className='container m-auto px-4 md:px-8 xl:px-0 text-white flex flex-col-reverse lg:flex-row lg:items-center lg:justify-between'>
-        <div className='flex flex-col gap-4  md:p-10 lg:p-0 w-full xl:1/2'>
-          <div className='flex justify-center p-6 md:p-0 md:justify-start'>
+    <div className='bg-gradient-to-b from-brandPurple-700 to-brandPurple-600 pt-4 md:pt-8 min-h-[calc(100vh-56px)] md:min-h-0  flex flex-col justify-around '>
+      <div className='container m-auto px-4 md:px-8 xl:px-0 text-white flex flex-col-reverse md:flex-row lg:items-center lg:justify-between'>
+        <div className='flex flex-col gap-2 md:gap-4 md:p-10 lg:p-0 w-full xl:1/2'>
+          <div className='flex justify-center p-4 md:p-0 md:justify-start'>
             <button className='bg-brandPurple-600 py-2 px-4 uppercase text-xs rounded-lg shadow-brandPurple-500 shadow-inner '>Do you belive in magic?</button>
           </div>
           <h1 className='text-6xl md:text-6xl lg:text-8xl xl:text-9xl font-normal max-w-[10ch]'>
@@ -22,19 +24,29 @@ function Hero() {
           </div>
           <div className='flex items-center text-white gap-1'>
             <TbArrowBigDownLines />
-            <p className='text-sm'>Scroll down for quick helper</p>
+            <p className='text-sm mb-2'>Scroll down for quick helper</p>
           </div>
         </div>
         <div className='w-full md:w-3/4 p-4 m-auto relative'>
           <img src={Housexmas} alt='3d House with christmas lights' className='w-full h-auto' />
         </div>
-        <div className='hidden lg:absolute -bottom-14 lg:bottom-24 right-8 lg:flex flex-col gap-4'>
+        {/* <div className='hidden lg:absolute -bottom-14 lg:bottom-24 right-8 lg:flex flex-col gap-4'>
           <ToastNotification />
           <ToastNotification updatetype='previuos' date='24 000m above Norway, March 5.2, T-12min' timepassed={10} />
-        </div>
+        </div> */}
       </div>
-      <div className='relative bottom-0 '>
+      <div className='relative top-1 '>
         <WavesSvg />
+        <div className='relative '>
+          <div className='absolute -top-[60px] md:-top-[200px]  lg:-top-[250px] xl:-top-[460px]'>
+            <div className='w-36 lg:w-44 xl:w-96 relative ml-4 rotate-12'>
+              <img src={Cookie} alt='galleta de gengibe' />
+            </div>
+            <div className='w-56 xl:w-96 translate-x-20 xl:translate-x-80 -translate-y-[80px] xl:-translate-y-[180px] '>
+              <img src={Letter} alt='carta' />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
